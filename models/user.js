5d4ce83d240658469e5e0381979e3dfaf2372ac3
Donald Lee <donaldlee50@gmail.com>
2016-08-08 21:45:44 -0500

    Episode 6  - app.js - Reconfigured the right db in mongo  - models/user.js - Replaced password attribute with salt and hash attribute  - routes/index.js - New User needed email attribute because it’s required  - views/index.jade - Indent and spaces problem; fixed  - login.jade - required a | for nbsp;  - register.jade - Added | for nbsp, and new email group.

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
   name: String,
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  salt: { type: String, required: true },
+  hash: { type: String, required: true },
   email: {type: String, required: true, unique: true},
   phone: Number,
   admin: Boolean,
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
-	User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+	User.register(new User({ username: req.body.username, email: req.body.email }), req.body.password, function(err, user) {
+		console.log(user);
 		if (err) {
 			return res.render('register', { user: user });
 		}
