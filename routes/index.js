74a52afc8d246ed6a0b982722e10e8bd136c19c2
Donald Lee <donaldlee50@gmail.com>
2016-08-09 18:54:37 -0500

    Episode 9:  - Added error handling. Modified following files:    - routes/index.js    - views/layout.jade

--- routes/index.js
+++ routes/index.js
@@ -14,13 +14,17 @@ router.get('/register', function(req, res) {
 
 router.post('/register', function(req, res) {
 	User.register(new User({ username: req.body.username, email: req.body.email }), req.body.password, function(err, user) {
-		console.log(user);
 		if (err) {
-			return res.render('register', { user: user });
+			return res.render('register', { error: err.message });
 		}
 
 		passport.authenticate('local')(req, res, function () {
-			res.redirect('/');
+			req.session.save(function (err) {
+				if (err) {
+					return next(err);
+				}
+				res.redirect('/');
+			});
 		});
 	});
 });
