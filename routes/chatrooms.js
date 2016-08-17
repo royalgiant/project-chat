491af8f0da5ff4d83ced1d608494cb32c23fe279
Donald Lee <donaldlee50@gmail.com>
2016-08-16 21:46:13 -0500

    Episode 22:       - db.js - deprecated       - chatrooms.js - moved db-related functions from db.js to chatroom.js       - messages.js - moved db-related functions from db.js to messages.js

--- routes/chatrooms.js
+++ routes/chatrooms.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db.js');
 var Chatroom = require('../models/chatroom.js')
 
 
 // Actual variables used are arbitrary at this point
+
 // List all chatrooms available
 router.get('/', function(req, res, next) {
 	console.log('got into routes')
 	Chatroom.find({}, function(err, chatroom) {
 	  	if (err) return console.error(err);
 	  	if (chatroom.length == 0) {
-	  		return "There are no chatrooms";
+	  		return res.send("There are no chatrooms");
 	  	} else {
 	  		console.log(chatroom)
 	  		return res.send(chatroom);
@@ -19,12 +19,33 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+// Create a new chatroom
 router.post('/insert', function(req, res, next) {
-	var chatroom = db.insertChatRoom(room_name, group_chat, users);
+	var chatroom = function(room_name, group_chat, users) {
+		users_names = []
+		user_ids = []
+		for (var user in users) {
+			users_names.push(user.name);
+			user_ids.push(user._id);
+		}
+		var chatroom = new Chatroom({room_name: room_name, 
+			group_chat: group_chat, 
+			users_names: users_names,
+			user_ids: user_ids
+		});
+		chatroom.save(function (err, chatroom) {
+			if (err) return console.error(err);
+			return res.send(chatroom);
+		});
+	}
 });
 
+// Get a specific chatroom
 router.post('/room/:id', function(req, res, next) {
-	var chatroom = db.insertChatRoom(req.params.id);
+	Chatroom.findOne({chatroom_id: chatroom_id}, function(err, chatroom){
+		if (err) return handleError(err);
+		return res.send(chatroom);
+	});
 });
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db.js');
-
+var Message = require('../models/message.js')
 
 // Actual variables used are arbitrary at this point
 // PROBLEM: We should probably limit the amount of messages we get because if we had 1 billion, that'd be alot of messages
 router.get('/', function(req, res, next){
-    if (req.query.chatroom) {
-        db.getMessages(req.query.chatroom, function(err, rows) {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send(rows);
-            }
+    if (req.query.chatroom && req.query.limit) {
+        Message.find({chatroom_id: chatroom_id}, function(err, messages) {
+            if (err) return console.error(err);
+            return res.send(messages);
         });
     } else {
         res.send('Invalid URL: no chat room or limit specified! Example of good URL: /messages?chatroom=general&limit=10&timezoneoffsethours=5');
     }
 });
+
+router.post('/insert', function(req, res, next) {
+    var message = function(message, user_name, chatroom_id) {
+        if (req.query.chatroom) {
+            var message = new Message({message: message, user_name: user_name, chatroom_id: chatroom_id});
+            message.save(function (err, message) {
+                if (err) return console.error(err);
+                return message;
+            });
+        } else {
+            res.send('Invalid URL: no chat room specified! Example of good URL: /messages?chatroom=general');
+        }
+    };
+}); 
 
 module.exports = router;
